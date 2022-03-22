5a29593c02d89438e52a79f561364cb3ffe34de1
Ferdinand Keller <ferdinandklr@gmail.com>
2022-03-22 22:37:58 +0100

    Dev2 (#1)
    * Update query2.rq
    
    * query3 + notation
    
    * query34
    
    * modif
    
    * 1
    
    Co-authored-by: Yunong Liu <77636286+Manaologysislow@users.noreply.github.com>

--- server.js
+++ server.js
@@ -43,9 +43,32 @@ function query1 (commune) {
   })
 }
 
-function query2 (commune) {
+function query2 (dispo) {
   return new Promise((resolve) => {
     readFile('./queries/query2.rq', 'utf-8', (_, query) => {
+      query = query.replace('#DISPO', dispo)
+      writeFile('./queries/tmp.rq', query, () => {
+        exec('sparql --data=datasets/ttl/data.ttl --query=queries/tmp.rq', (_, stdout) => {  
+          let lines = stdout.split('\n').slice(3, -2)
+          let data = lines.map(line => {
+            let split = line.split('|').slice(1, -1).map(x => x.trim())
+            let fontaine = split[0]
+            let commune = split[1].split('"')[1]
+            let disponible = split[2].split('"')[2]
+            let latitude = parseFloat(split[3].split('"')[1])
+            let longitude = parseFloat(split[4].split('"')[1])
+            return { fontaine, commune, voie, disponible, latitude, longitude }
+          })
+          resolve(data)
+        })
+      })
+    })
+  })
+}
+
+function query3 () {
+  return new Promise((resolve) => {
+    readFile('./queries/query3.rq', 'utf-8', (_, query) => {
       //query = query.replace('#COMMUNE', commune)
       //writeFile('./queries/tmp.rq', query, () => {
         //exec('sparql --data=datasets/ttl/data.ttl --query=queries/tmp.rq', (_, stdout) => {  
@@ -67,6 +90,21 @@ function query2 (commune) {
   })
 }
 
+function query4 (commune) {
+  return new Promise((resolve) => {
+    readFile('./queries/query4.rq', 'utf-8', (_, query) => {
+      query = query.replace('#COMMUNE', commune)
+      writeFile('./queries/tmp.rq', query, () => {
+        exec('sparql --data=datasets/ttl/data.ttl --query=queries/tmp.rq', (_, stdout) => {  
+          let lines = stdout.split('\n').slice(3, -2)
+          
+          resolve(lines)
+        })
+      })
+    })
+  })
+}
+
 // WEBSITE CODE
 let app = express()
 let port = 8080
@@ -80,6 +118,17 @@ app.get('/api/query0', async (req, res) => {
 
 app.get('/api/query1/:arrondissement', async (req, res) => {
   let fontaines = await query1('PARIS ' + req.params.arrondissement.toString() + (req.params.arrondissement==1?'ER':'EME') +  ' ARRONDISSEMENT')
+  res.json(fontaines)
+})
+
+app.get('/api/query2/:dispo', async (req, res) => {
+  let fontaines = await query2(req.params.dispo == 0 ? 'NON' : 'OUI')
+  res.json(fontaines)
+})
+
+app.get('/api/query4/:arrondissement', async (req, res) => {
+  let fontaines = await query4(req.params.arrondissement.toString())
+  //let fontaines = await query4('PARIS ' + req.params.arrondissement.toString() + (req.params.arrondissement==1?'ER':'EME') +  ' ARRONDISSEMENT')
   res.json(fontaines)
 })
 
