fb71db0b9a4382450c7f117d9176f50f240d2a68
Ferdinand Keller <ferdinandklr@gmail.com>
2022-03-23 15:34:23 +0100

    Dev1 (#5)
    * ✨ UI fonctionnelle
    
    * ✨ added ttl export
    
    * MAIN (#3)
    
    * Dev2 (#1)
    
    * Update query2.rq
    
    * query3 + notation
    
    * query34
    
    * modif
    
    * 1
    
    Co-authored-by: Yunong Liu <77636286+Manaologysislow@users.noreply.github.com>
    
    * Dev1 (#2)
    
    * ✨ UI fonctionnelle
    
    * ✨ added ttl export
    
    Co-authored-by: Yunong Liu <77636286+Manaologysislow@users.noreply.github.com>
    
    * ✨ ui working
    
    * ✨ deployment
    
    Co-authored-by: Yunong Liu <77636286+Manaologysislow@users.noreply.github.com>

--- website/script.js
+++ website/script.js
@@ -30,11 +30,25 @@ let off_style = new ol.style.Style({
     radius: 8,
   })
 })
+let wifi_style = new ol.style.Style({
+  image: new ol.style.Circle({
+    fill: new ol.style.Fill({
+      color: 'rgb(133, 179, 23, 0.7)'
+    }),
+    radius: 8,
+  })
+})
 
 // add function for clearing layers
 let current_layers = []
 function clear_previous_layers() {
   for (let layer of current_layers) {
+    map.removeLayer(layer)
+  }
+}
+let current_layers_wifi = []
+function clear_previous_layers_wifi() {
+  for (let layer of current_layers_wifi) {
     map.removeLayer(layer)
   }
 }
@@ -61,6 +75,27 @@ async function query_all() {
   map.addLayer(layer)
   current_layers.push(layer)
 }
+async function query_all_wifi() {
+  clear_previous_layers_wifi()
+
+  let resp = await fetch('api/query0wifi')
+  let wifis = await resp.json()
+  
+  let features = []
+  for (let wifi of wifis) {
+    let f = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.fromLonLat([wifi.longitude, wifi.latitude])),
+    })
+    f.setStyle(wifi_style)
+    features.push(f)
+  }
+  
+  var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({ features }),
+  })
+  map.addLayer(layer)
+  current_layers_wifi.push(layer)
+}
 
 async function query_arr(arr) {
   clear_previous_layers()
@@ -82,6 +117,71 @@ async function query_arr(arr) {
   })
   map.addLayer(layer)
   current_layers.push(layer)
+}
+async function query_arr_wifi(arr) {
+  clear_previous_layers_wifi()
+
+  let resp = await fetch(`api/query1wifi/${arr}`)
+  let fontaines = await resp.json()
+  
+  let features = []
+  for (let fontaine of fontaines) {
+    let f = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.fromLonLat([fontaine.longitude, fontaine.latitude])),
+    })
+    f.setStyle(wifi_style)
+    features.push(f)
+  }
+  
+  var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({ features }),
+  })
+  map.addLayer(layer)
+  current_layers_wifi.push(layer)
+}
+
+async function query_dispo(dispo) {
+  clear_previous_layers()
+
+  let resp = await fetch(`api/query2/${dispo}`)
+  let fontaines = await resp.json()
+
+  let features = []
+  for (let fontaine of fontaines) {
+    let f = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.fromLonLat([fontaine.longitude, fontaine.latitude])),
+    })
+    f.setStyle(fontaine.disponible == 'OUI' ? on_style : off_style)
+    features.push(f)
+  }
+  
+  var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({ features }),
+  })
+  map.addLayer(layer)
+  current_layers.push(layer)
+}
+
+async function queryoptwifi() {
+  clear_previous_layers_wifi()
+
+  let resp = await fetch(`api/queryoptwifi`)
+  let wifis = await resp.json()
+  
+  let features = []
+  for (let wifi of wifis) {
+    let f = new ol.Feature({
+      geometry: new ol.geom.Point(ol.proj.fromLonLat([wifi.longitude, wifi.latitude])),
+    })
+    f.setStyle(wifi_style)
+    features.push(f)
+  }
+  
+  var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({ features }),
+  })
+  map.addLayer(layer)
+  current_layers_wifi.push(layer)
 }
 
 async function query_dispo(dispo) {
@@ -113,8 +213,10 @@ arr_btn.addEventListener('click', () => {
   let arr = parseInt(arr_in.value)
   if (isNaN(arr) || arr < 0 || arr > 20) {
     query_all()
+    query_all_wifi()
   } else {
     query_arr(encodeURI('PARIS ' + arr.toString() + (arr == 1 ? 'ER' : 'EME') + ' ARRONDISSEMENT'))
+    query_arr_wifi(encodeURI('PARIS ' + arr.toString() + (arr == 1 ? 'ER' : 'EME') + ' ARRONDISSEMENT'))
   }
 })
 
@@ -124,5 +226,11 @@ dispo_btn.addEventListener('click', () => {
   query_dispo(encodeURI(dispo_in.value))
 })
 
+let opt_btn = document.getElementById('opt_btn')
+opt_btn.addEventListener('click', () => {
+  queryoptwifi()
+})
+
 // load everything by default
 query_all()
+query_all_wifi()
